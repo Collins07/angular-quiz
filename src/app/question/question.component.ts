bad0665ac5785dbca7586603fb60f2465e5a6a82
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-08 09:38:33 +0300

    angular quiz project

---
+++ src/app/question/question.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../service/question.service';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css']
+})
+export class QuestionComponent implements OnInit {
+  public name:string = "";
+  public questionList : any = [];
+  public currentQuestion : number = 0
+
+  constructor(private questionService : QuestionService) { }
+
+  ngOnInit(): void {
+    this.name = localStorage.getItem("name")!;
+    this.getAllQuestions();
+  }
+  getAllQuestions(){
+    this.questionService.getQuestionJson()
+    .subscribe(res=>{
+    this.questionList = res.questions;
+    })
+
+    }
+  }
+
+
